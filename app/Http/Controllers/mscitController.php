ca33024eafe551e48b80e9156bfac463fb1508fd
foram2312 <dhananiravi.vits@gmail.com>
2024-08-05 08:45:26 +0530

    Make controller and routes

---
+++ app/Http/Controllers/mscitController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mscitController extends Controller
+{
+    public function printHello(){
+        echo "Hello From MSC IT";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\mscitController;
+use App\Http\Controllers\DemoController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/student', function () {
-    return view('student');
+Route::get('/sayhello', function(){
+    // echo "test";
+    for($i=1; $i<=10; $i++)
+        echo $i;
+    // return ('this is my say hello');
 });
+Route::get('demo',[DemoController::class,'printHello']);
+
+// Route::get('/student', function () {
+//     return view('student');
+// });
 
 // Route::get('/login', function () {
 //     return view('login');
 // });
-
-// Route::get("testRoute",[Test])
